1e0c088245b456c2e39027e7623662a891b39ec2
Sergii <nezalonov.serhii@gmail.com>
2023-07-06 05:20:30 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 function App() {
    return (
       <>
-         <Routes></Routes>
          <ToastContainer />
       </>
    );
