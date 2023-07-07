cb0dae42e359be83877ced90365c69b6d2e17e3f
Zhenya Plyasova <111362107+Zhenya-Plyasova@users.noreply.github.com>
2023-07-07 19:03:48 +0300

    router logic added

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
-import { Header } from "components/Header/Header";
+import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
 import Loader from "../Loader/Loader";
 
