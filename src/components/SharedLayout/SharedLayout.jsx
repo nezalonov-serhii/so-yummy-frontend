9b894cc40242969ed92d023524d7cfe0b0e82919
Sergii <nezalonov.serhii@gmail.com>
2023-07-09 17:06:02 +0300

    Fixed: nav bar styled

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,23 +2,22 @@ import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
 import { Header } from "../Header/Header";
-// import { Footer } from "../Footer/Footer";
 import Loader from "../Loader/Loader";
 import { useSelector } from "react-redux";
 import { selectToken } from "../../redux/selector/selectors";
 import Footer from "./Footer/Footer";
 
 const SharedLayout = () => {
-  const isAuthorize = useSelector(selectToken);
-  return (
-    <>
-      {isAuthorize && <Header />}
-      <Suspense fallback={<Loader />}>
-        <Outlet />
-      </Suspense>
-      {isAuthorize && <Footer />}
-    </>
-  );
+   const isAuthorize = useSelector(selectToken);
+   return (
+      <>
+         {isAuthorize && <Header />}
+         <Suspense fallback={<Loader />}>
+            <Outlet />
+         </Suspense>
+         {isAuthorize && <Footer />}
+      </>
+   );
 };
 
 export default SharedLayout;
