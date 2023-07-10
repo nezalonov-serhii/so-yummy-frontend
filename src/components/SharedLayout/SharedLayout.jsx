febd8aa8753472c979319ca089f569a34d80dd8b
Sergii <nezalonov.serhii@gmail.com>
2023-07-10 17:20:23 +0300

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "../Header/Header";
 import Loader from "../Loader/Loader";
+import Background from "../Background";
 import { useSelector } from "react-redux";
 import { selectToken } from "../../redux/selector/selectors";
 
