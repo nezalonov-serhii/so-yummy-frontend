a6426be7a392e2c1fc52434197e9f87b815b4a0a
Viktoria <vik25@i.ua>
2023-07-10 13:02:22 +0200

    add FollowUsAddRecipe

---
+++ src/components/AddRecipe/FollowUsAddRecipe/FollowUsAddRecipe.jsx
@@ -0,0 +1,10 @@
+import { FollowUs } from "../../Footer/FollowUs/FollowUs";
+
+export const FollowUsAddRecipe = () => {
+  return (
+    <div>
+      <h4>FollowUs</h4>
+      <FollowUs />
+    </div>
+  );
+};
