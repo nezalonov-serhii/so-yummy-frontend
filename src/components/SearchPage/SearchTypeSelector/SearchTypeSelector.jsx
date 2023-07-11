a812a454b6c1f9e59ccba3d802096993efbdcb63
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-07-11 19:02:42 +0300

    СHANGE: SearchTypeSelectr component, add MUI logic

--- src/components/SearchPage/SearchTypeSelector/SearchTypeSelector.jsx
+++ src/components/SearchPage/SearchTypeSelector/SearchTypeSelector.jsx
@@ -1,20 +1,45 @@
-import {SearchSelectorBox,SearchSelectorLabel,  SearchSelector
-} from "./SearchTypeSelector.styled"
+import { useSearchParams } from 'react-router-dom';
+import {SearchSelect,SearchSelectorBox,SelectorOption,StyledHelperText,StyledFormControl} from "./SearchTypeSelector.styled";
+import FormControl from '@mui/material/FormControl';
+
 
 export const SearchTypeSelector = () => {
-    return (
-        
-        <SearchSelectorBox>
-       <SearchSelectorLabel htmlFor="type">Search by:</SearchSelectorLabel>
 
-            {/* <SelectorBox> */}
-            <SearchSelector id="type" name="type">
-  <option value="title">Title</option>
-  <option value="ingridients">Ingridients</option>
-  
-                </SearchSelector>
-                 {/* </SelectorBox> */}
-            </SearchSelectorBox>
-           
-    )
-}
+  //   const [searchSelectedParams, setSearchSelectedParams] = useSearchParams();
+  //   const submitSlectedSearch = e => {
+  //     console.log(e.target.value)
+  //  const  type = e.target.value
+  //   e.preventDefault();
+    
+  //   setSearchSelectedParams({ type: "Beef" });
+  //   form.reset();
+  //   };
+  // const query = searchSelectedParams.get("query")
+  // const ingredients = searchSelectedParams.get("ingredients")
+    return (
+    // <SearchSelectorBox sx={{ maxWidth: 146 }}>
+     <SearchSelectorBox >
+            <FormControl fullWidth>
+                <StyledFormControl>
+                 <StyledHelperText id="username-helper">
+                   Search by:
+                </StyledHelperText>
+                {/* <InputLabel id="demo-simple-select-label">{query}</InputLabel> */}
+                {/* <InputLabel variant="standard" >{query}</InputLabel> */}
+               
+        <SearchSelect
+        //   labelId="demo-simple-select-label"
+        //   id="demo-simple-select"
+          defaultValue={"Title"}
+        
+        
+        >
+          <SelectorOption value="Title" >Title</SelectorOption>
+          <SelectorOption  value="Ingridients">Ingridients</SelectorOption>
+          
+                    </SearchSelect>
+                    </StyledFormControl>
+      </FormControl>
+    </SearchSelectorBox>
+        )
+ }
