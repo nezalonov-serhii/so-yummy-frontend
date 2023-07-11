dcb3bade25b5c79a45e77bf2f0e2523e689b2e17
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-07-11 12:46:08 +0300

    ADD: validation with formik and Yup in SubmitForm

--- src/components/SearchPage/SearchForm/SearchForm.jsx
+++ src/components/SearchPage/SearchForm/SearchForm.jsx
@@ -1,24 +1,66 @@
-import { SearchFormbox,SearchFormInput,SearchFormButton } from "./SearchForm.styled";
+import { useSearchParams } from 'react-router-dom';
+import { SearchFormBox, SearchFormInput, SearchFormButton,ErrorText } from "./SearchForm.styled";
+// import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik} from 'formik';
+// import { object, string, number, date, InferType } from 'yup';
+import * as Yup from 'yup';
 
+const userSchema = Yup.object({
+       query: Yup.string().matches(/^[A-Za-z\s_-]+$/,"Invalid query").required('Please fill field'),
+      });
+
+const initialValue = {
+  query:"",
+}
 export const SearchForm = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const handleSubmit = (values,  {resetForm} ) => {
   
+    console.log(values)
+ 
+    resetForm()
+  }
+  //   const submitSearch = e => {
+  //   e.preventDefault();
+  //   const form = e.currentTarget;
+  //   setSearchParams({ query: form.elements.query.value });
+  //   form.reset();
+  // };
+
+  // const inputSearch = e => {
+       
+  //   // e.preventDefault();
+  //   const input = e.currentTarget;
+  //   setSearchParams({ query: input.value });
+
+    // form.reset();
+  // };
+  const query = searchParams.get("query")
   return (
+   
+    <Formik initialValues={initialValue} onSubmit={handleSubmit} validationSchema = {userSchema} >
+    
+    <SearchFormBox>
      
-    <SearchFormbox>
-      {/* <fieldset > */}
-      <SearchFormInput
-        type="text"
-        name="searchInput"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        value="Beef"
+     
+        <SearchFormInput type="text" name="query"
+      
+        placeholder="Enter the text"
+        value={query}
+
         // onChange={this.handleStateInput}
         // id="inputName"
         // required
         
-      />
+        />
+        
+      <ErrorText name="query" component="div"/>
         <SearchFormButton type="submit">Search</SearchFormButton>
-        {/* </fieldset> */}
-    </SearchFormbox>
+      
+      </SearchFormBox>
+      
+    
+       </Formik>
   );
 };
+
