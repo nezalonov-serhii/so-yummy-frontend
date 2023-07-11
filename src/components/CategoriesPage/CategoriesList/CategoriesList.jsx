918a1b72e5a8438d4cd0c203eaf8d275af694c93
OlesiaKryvosheieva <koi.23102001@gmail.com>
2023-07-11 11:14:07 +0200

    added css to CategoriesList

--- src/components/CategoriesPage/CategoriesList/CategoriesList.jsx
+++ src/components/CategoriesPage/CategoriesList/CategoriesList.jsx
@@ -1,20 +1,21 @@
 
 import categories from "../../../service/api/categories.json"
-import { Box,Tab, Tabs} from "@mui/material";
-import { List  } from "./CategoriesList.styled";
+
+import { List, Button,Item  } from "./CategoriesList.styled";
 
 
 
-const CategoriesList = ({onSubmit, recipes}) => {
+const CategoriesList = ({onSubmit}) => {
 
     return <>
     <List>
     {categories.map(category => {
-        return  <Box sx={{ borderBottom: 1, borderColor: 'rgba(224, 224, 224, 1)'}}>
-  <Tabs  key={category._id} >
-    <Tab label={category.name} onClick = {() => onSubmit(category.name)} sx={{padding:" 10px 0px 32px 29px", color:"rgba(224, 224, 224, 1)", fontFamily:"Poppins", fontWeight:"400", textTransform:"capitalize" }} />
-  </Tabs>
-</Box> })}
+        return  <Item  key={category._id} >
+    <Button  onClick = {() => onSubmit(category.name)} >
+    {category.name}
+    </Button>
+  </Item>
+ })}
     </List>
     </>
      
--- src/components/CategoriesPage/CategoriesList/CategoriesList.styled.js
+++ src/components/CategoriesPage/CategoriesList/CategoriesList.styled.js
@@ -3,9 +3,56 @@ import styled from "styled-components";
 export const List = styled.ul`
   display: flex;
   flex-direction: row;
-  padding: 0 15px;
+  margin-left: 16px;
   width: 100%;
   overflow-x: auto;
-
+  border-bottom: 1px solid rgba(224, 224, 224, 1);
   margin-bottom: 32px;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  @media (min-width: 768px) {
+    margin-bottom: 50px;
+  }
+  @media (min-width: 1280px) {
+    margin-bottom: 50px;
+  }
+`;
+
+export const Item = styled.li`
+  padding: 10px 0px 32px 29px;
+
+  @media (min-width: 768px) {
+    padding: 10px 0px 28px 30px;
+  }
+
+  &:not(:first-child) {
+    @media (min-width: 768px) {
+      margin-left: 25px;
+    }
+
+    @media (min-width: 1280px) {
+      margin-left: 0;
+      padding: 10px 0px 28px 10px;
+    }
+  }
+`;
+
+export const Button = styled.button`
+  font-family: Poppins;
+  border: none;
+  font-weight: 400;
+  background-color: inherit;
+  color: rgba(224, 224, 224, 1);
+  font-size: 14px;
+  @media (min-width: 768px) {
+    font-size: 18px;
+    color: rgba(189, 189, 189, 1);
+  }
+
+  @media (min-width: 1280px) {
+    font-size: 16px;
+  }
 `;
