4383e1eeb9a92ca0fd62509766c05bff643ba557
Taisiya Tomashevska <taya22011985@gmail.com>
2023-07-12 20:09:54 +0300

    fix

--- src/components/Footer/Footer.Styled.js
+++ src/components/Footer/Footer.Styled.js
@@ -1,11 +1,29 @@
 import styled from "styled-components";
 
+export const Section = styled.div`
+  background-color: var(--background-footer);
+`
+
+export const Wrapper = styled.div`
+   max-width: 480px;
+    margin: 0 auto;
+   padding: 0 16px;
+   
+
+   @media screen and (min-width: 768px) {
+      max-width: 768px;
+   }
+   @media screen and (min-width: 1280px) {
+      max-width: 1280px;
+   }
+`;
+
 export const FooterWrapper = styled.div`
   margin: 0 auto;
   width: 100%;
   height: 440px;
   padding-top: 28px;
-  background-color: var(--background-footer);
+
 
   @media (min-width: 768px) {
     padding-top: 50px;
