1bad170ab21c870464786d5314cdee74148d961b
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-07-13 18:55:45 +0300

    ADD: useStete "RecipeList"

--- src/page/SearchPage/SearchPage.jsx
+++ src/page/SearchPage/SearchPage.jsx
@@ -1,11 +1,11 @@
+import { useEffect,useState } from "react";
 import { Title } from "../../components/Title/Title"
 import { SearchBar } from "../../components/SearchPage/SearchBar/SearchBar"
 import { SearchedRecipesList } from "../../components/SearchPage/SearchedRecipesList/SearchedRecipesList"
 import { SearchPageContainer } from "./SearchPage.styled";
-
-
+import { searchByTitle } from "../../service/api/apiSearch";
 const SearchPage = () => {
-  
+  const [recipeList, setrecipeList] = useState('');
   return (
     <SearchPageContainer>
     <Title>Search</Title>
