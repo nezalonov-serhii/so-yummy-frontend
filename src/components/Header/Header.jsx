11f7c16d71e5b8299897c5a528d094067c10e728
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-07-12 23:00:36 -0700

    add style burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { PiForkKnifeBold, PiListBold } from "react-icons/pi";
+import { IoIosSearch } from "react-icons/io";
 import { CgClose } from "react-icons/cg";
 import  Container from "../Container";
 
@@ -10,12 +11,16 @@ import {
   StyledLink,
   Box,
   MobileBtn,
-  Logo,
-
+  Logo,Search
 } from "./styled";
 
 export const Header = () => {
   const [nav, setNav] = useState(false);
+  // const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // const toggleTheme = () => {
+  //   setIsDarkMode(prevMode => !prevMode);
+  // };
 
   return (
     <Container>
@@ -25,32 +30,40 @@ export const Header = () => {
             <PiForkKnifeBold width={40} height={40} color="white" />
           </Logo>
           <Nav>
-          <nav>
-            <Line nav={nav}>
-              <li>
-                <StyledLink to="/categories">Categories</StyledLink>
-              </li>
-              <li>
-                <StyledLink to="/add">Add Recipe</StyledLink>
-              </li>
-              <li>
-                <StyledLink to="/recipe">My Recipe</StyledLink>
-              </li>
-              <li>
-                <StyledLink to="/favorite">Favorite</StyledLink>
-              </li>
-              <li>
-                <StyledLink to="/shopping-list">Shopping list</StyledLink>
-              </li>
-              <li>
-                <StyledLink to="/search">Search</StyledLink>
-              </li>
-            </Line>
-            <MobileBtn onClick={() => setNav(!nav)}>
-              {nav ? <CgClose /> : <PiListBold />}
-            </MobileBtn>
-          </nav>
+            <nav>
+              <Line nav={nav}>
+                <li>
+                  <StyledLink to="/categories">Categories</StyledLink>
+                </li>
+                <li>
+                  <StyledLink to="/add">Add Recipe</StyledLink>
+                </li>
+                <li>
+                  <StyledLink to="/recipe">My Recipe</StyledLink>
+                </li>
+                <li>
+                  <StyledLink to="/favorite">Favorite</StyledLink>
+                </li>
+                <li>
+                  <StyledLink to="/shopping-list">Shopping list</StyledLink>
+                </li>
+                <Search>
+                <IoIosSearch  width={20} height={20}/>
+                  <StyledLink to="/search"> Search</StyledLink>
+                </Search>
+              </Line>
+              <MobileBtn onClick={() => setNav(!nav)}>
+                {nav ? <CgClose /> : <PiListBold />}
+              </MobileBtn>
+            </nav>
           </Nav>
+          {/* <ThemeSwitch>
+            <ThemeToggle
+              type="checkbox"
+              checked={isDarkMode}
+              onChange={toggleTheme}
+            />
+          </ThemeSwitch> */}
         </Box>
       </HeaderContainer>
     </Container>
