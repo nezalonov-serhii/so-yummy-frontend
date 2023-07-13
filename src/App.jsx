ba5a7a8c1e759226ba41e8544565264bd75856ce
Zhenya Plyasova <111362107+Zhenya-Plyasova@users.noreply.github.com>
2023-07-13 18:59:36 +0300

    error page corrected

--- src/App.jsx
+++ src/App.jsx
@@ -147,7 +147,7 @@ function App() {
              }
            />
            <Route
-             path="page-404"
+             path="*"
              element={
                <Suspense>
                  <PrivateRoute>
