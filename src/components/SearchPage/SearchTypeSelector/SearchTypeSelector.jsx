76331028da0289082d15488c950a122f949a129e
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-07-14 20:24:30 +0300

    DELETE: commented-out code

--- src/components/SearchPage/SearchTypeSelector/SearchTypeSelector.jsx
+++ src/components/SearchPage/SearchTypeSelector/SearchTypeSelector.jsx
@@ -6,31 +6,19 @@ import { selectSearchValue } from '../../../redux/selector/selectors';
 
 export const SearchTypeSelector = () => {
 const valueSelector = useSelector(selectSearchValue);
-  //   const [searchSelectedParams, setSearchSelectedParams] = useSearchParams();
-  //   const submitSlectedSearch = e => {
-  //     console.log(e.target.value)
-  //  const  type = e.target.value
-  //   e.preventDefault();
-    
-  //   setSearchSelectedParams({ type: "Beef" });
-  //   form.reset();
-  //   };
-  // const query = searchSelectedParams.get("query")
-  // const ingredients = searchSelectedParams.get("ingredients")
+  
     return (
-    // <SearchSelectorBox sx={{ maxWidth: 146 }}>
+    
      <SearchSelectorBox >
             <FormControl fullWidth>
                 <StyledFormControl>
                  <StyledHelperText id="username-helper">
                    Search by:
                 </StyledHelperText>
-                {/* <InputLabel id="demo-simple-select-label">{query}</InputLabel> */}
-                {/* <InputLabel variant="standard" >{query}</InputLabel> */}
+             
                
         <SearchSelect
-        //   labelId="demo-simple-select-label"
-        //   id="demo-simple-select"
+      
           defaultValue={"Title"}
          value = {valueSelector??"Title"}
         
