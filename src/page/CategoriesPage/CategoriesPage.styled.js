2f5852ca4119525e22be94cee63384a8812cb7d3
OlesiaKryvosheieva <koi.23102001@gmail.com>
2023-07-15 14:53:35 +0200

    added padding to section

--- src/page/CategoriesPage/CategoriesPage.styled.js
+++ src/page/CategoriesPage/CategoriesPage.styled.js
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  padding-bottom: 100px;
+  @media (min-width: 768px) {
+    padding-bottom: 200px;
+  }
   @media (min-width: 1280px) {
     margin: 0 auto;
     width: 1280px;
     padding: 0 20px;
+    padding-bottom: 200px;
   }
 `;
