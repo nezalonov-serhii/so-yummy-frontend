4fe9c96d9552003d9b969342ae5baac8adf974c8
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-07-15 12:17:26 +0300

    CHANGE: add useState and change useEffect logic

--- src/page/SearchPage/SearchPage.jsx
+++ src/page/SearchPage/SearchPage.jsx
@@ -1,24 +1,45 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
+import {useSelector } from "react-redux";
 import { Title } from "../../components/Title/Title"
 import { SearchBar } from "../../components/SearchPage/SearchBar/SearchBar"
 import { SearchedRecipesList } from "../../components/SearchPage/SearchedRecipesList/SearchedRecipesList"
 import { SearchPageContainer } from "./SearchPage.styled";
-import { searchByTitle } from "../../service/api/apiSearch";
-// import { useSearchParams } from "react-router-dom";
+import { searchByTitle, searchByIngridients } from "../../service/api/apiSearch";
+import { selectSearchValue } from '../../redux/selector/selectors';
+
+
 
 const SearchPage = () => {
-    // const [searchParams, setSearchParams] = useSearchParams();
-  //  const query = searchParams.get("query");
+  const valueSelector = useSelector(selectSearchValue);
+  const [searchFormValue, setSearchFormValue] = useState('');
+    const handleSearchFormInput = (inputValue) => {
+   setSearchFormValue(inputValue)
+  };
+  
+   
   const [recipeList, setrecipeList] = useState('');
-    useEffect(() => {
-    searchByTitle({ query: "Beef" }).then(({data}) => setrecipeList(data.data))
-    },[]
-    // [recipeList]
+  useEffect(() => {
+    
+    if (searchFormValue === "") {
+      
+      return 
+    }
+    if (valueSelector === "Title") {
+      console.log("Search By Title",searchFormValue)
+      searchByTitle({ query: searchFormValue }).then(({ data }) => setrecipeList(data.data)).finally(setSearchFormValue(""))
+    }
+    else {
+      console.log("Search By Ingridients",searchFormValue)
+      searchByIngridients({ query: searchFormValue }).then(({ data }) => setrecipeList(data.data)).finally(setSearchFormValue(""))
+    }
+  },
+    [searchFormValue]
+    // [searchFormValue, valueSelector]
   );
   return (
     <SearchPageContainer>
     <Title>Search</Title>
-    <SearchBar />
+    <SearchBar handleSearchFormInput={handleSearchFormInput} />
     <SearchedRecipesList listOfRecipes={recipeList} />
     </SearchPageContainer>
   
