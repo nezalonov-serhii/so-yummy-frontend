706e3d7ded311dc4c1c10cd39afe23e83009435c
OlesiaKryvosheieva <koi.23102001@gmail.com>
2023-07-16 10:47:31 +0200

    solved problem with defaultImg

--- src/components/CategoriesPage/CategoriesGallery/CategoriesGallery.jsx
+++ src/components/CategoriesPage/CategoriesGallery/CategoriesGallery.jsx
@@ -5,11 +5,14 @@ import { Title, Item, Picture, List } from "./CategoriesGallery.styled";
 import defaultImage from "../../../images/DefaultImage/defaultImage.svg"
 
 const CategoriesGallery = ({recipes}) => {
-   
+    const onImageError = (e) => {
+        e.target.src = defaultImage
+      }
+    
 
     return <List>
     {recipes.map(recipe => {
-        return <Item key={recipe._id}><Link to={`/recipes/${recipe._id}`}><Title>{recipe.title}</Title><Picture src={recipe.preview || defaultImage}/></Link></Item>
+        return <Item key={recipe._id}><Link to={`/recipes/${recipe._id}`}><Title>{recipe.title}</Title><Picture src={recipe.preview? recipe.preview : defaultImage} onError={onImageError}/></Link></Item>
     })}
 
     </List>
