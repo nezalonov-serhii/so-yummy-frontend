85c07175d69dd8a2bdcc6558913c27ee0d0e88d8
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-07-17 22:49:32 -0700

    update styles modal

--- src/components/Header/Navigate/Navigate.jsx
+++ src/components/Header/Navigate/Navigate.jsx
@@ -14,70 +14,76 @@ import {
   Name,
   BoxName,
   LogoAvatar,
-  Box
+  Box,
 } from "./Navigate.styled";
 import { ModalEditProfile } from "../ModalEditProfile/ModalEditProfile";
 import { Logout } from "../Logout/Logout";
+import { UserProfile } from "../UserProfile/UserProfile";
 
 export const Navigate = () => {
-   const [nav, setNav] = useState(false);
-   const [modalActive, setModalActive] = useState(false);
-   const [modalLogoutActive, setModalLogoutActive] = useState(false);
-   const user = useSelector(selectUser);
+  const [nav, setNav] = useState(false);
+  const [modalActive, setModalActive] = useState(false);
 
-   const handleAvatarClick = () => {
-      setModalActive(true);
-   };
+  const [modalLogoutActive, setModalLogoutActive] = useState(false);
+  const [userProfileActive, setUserProfileActive] = useState(false);
+  const user = useSelector(selectUser);
 
-   const closeModal = () => {
-      setModalActive(false);
-      setModalLogoutActive(false);
-   };
+  const handleAvatarClick = () => {
+    setModalActive(true);
+  };
 
+  const closeModal = (setter) => {
+    setter(false);
+  };
 
   return (
     <Nav>
-      
-        <Line nav={nav}>
-          <li>
-            <StyledLink to="/categories/breakfast">Categories</StyledLink>
-          </li>
-          <li>
-            <StyledLink to="/add">Add Recipe</StyledLink>
-          </li>
-          <li>
-            <StyledLink to="/my">My Recipe</StyledLink>
-          </li>
-          <li>
-            <StyledLink to="/favorite">Favorite</StyledLink>
-          </li>
-          <li>
-            <StyledLink to="/shopping-list">Shopping list</StyledLink>
-          </li>
-          <Search>
-            <StyledLink to="/search">
-              <FiSearch width={24} height={24} />
-              <SearchText>Search</SearchText>
-            </StyledLink>
-          </Search>
-        </Line>
-        <Box>
+      <Line nav={nav}>
+        <li>
+          <StyledLink to="/categories/breakfast">Categories</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/add">Add Recipe</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/my">My Recipe</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/favorite">Favorite</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/shopping-list">Shopping list</StyledLink>
+        </li>
+        <Search>
+          <StyledLink to="/search">
+            <FiSearch width={24} height={24} />
+            <SearchText>Search</SearchText>
+          </StyledLink>
+        </Search>
+      </Line>
+      <Box>
         <BoxName onClick={handleAvatarClick}>
-        <LogoAvatar />
-        <Name>{user?.name}</Name>
-      </BoxName>
+          <LogoAvatar />
+          <Name>{user?.name}</Name>
+        </BoxName>
         <MobileBtn size={32} onClick={() => setNav(!nav)}>
           {nav ? <CgClose size={32} /> : <HiOutlineMenuAlt2 size={32} />}
         </MobileBtn>
-        {modalActive && <ModalEditProfile closeModal={closeModal} />}
-        </Box>
-        
-      
-      
-      {modalLogoutActive && <Logout closeModal={closeModal} />}
+        {modalActive && (
+          <ModalEditProfile
+            setModalLogoutActive={setModalLogoutActive}
+            setUserProfileActive={setUserProfileActive}
+            setModalActive={setModalActive}
+          />
+        )}
+      </Box>
+
+      {modalLogoutActive && (
+        <Logout closeModal={() => closeModal(setModalLogoutActive)} />
+      )}
+      {userProfileActive && (
+        <UserProfile closeModal={() => closeModal(setUserProfileActive)} />
+      )}
     </Nav>
   );
-
 };
-
-export default Navigate;
