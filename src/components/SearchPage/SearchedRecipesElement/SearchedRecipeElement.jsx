b8e3f4c767479563a8a242e29242ab22ddd81fcd
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-07-18 18:28:41 +0300

    ADD: defaultImage to SearchRecipeElement

--- src/components/SearchPage/SearchedRecipesElement/SearchedRecipeElement.jsx
+++ src/components/SearchPage/SearchedRecipesElement/SearchedRecipeElement.jsx
@@ -1,13 +1,13 @@
 import { RecipeImage, RecipeTitle } from "./SearchedRecipeElement.styled";
 import { NavLink } from "react-router-dom";
-
+import defaultImage from "../../../images/DefaultImage/defaultImage.svg"
 export const SearchedRecipeElement = ({ title, recipeId, img, desc }) => {
 
    return (
     <NavLink to={`/recipe/${recipeId}`}>
-            <RecipeImage src={img} alt={desc} />
+            <RecipeImage src={img?img: defaultImage} alt={desc} />
             <RecipeTitle>{title}</RecipeTitle>
          </NavLink>
-     
+   
    );
 };
