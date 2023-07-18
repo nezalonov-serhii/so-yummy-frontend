6cf3299c44a74e89c58ca18896266681b4385864
zoria93 <artemzora4@gmail.com>
2023-07-18 15:27:38 +0300

    add checkbox

--- src/redux/shopping/sliceShopping.js
+++ src/redux/shopping/sliceShopping.js
@@ -1,73 +1,70 @@
-import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from "@reduxjs/toolkit";
 import {
-    deleteShoppingThunk,
-    getShoppingThunk,
-    addShoppingThunk,
-} from './thunkShopping';
+  deleteShoppingThunk,
+  getShoppingThunk,
+  addShoppingThunk,
+} from "./thunkShopping";
 
+const heandleFulfilledAdd = (state, { payload }) => {
+  state.shopping.isLoading = false;
+  state.shopping.items = [...payload];
+  state.shopping.error = "";
+};
 
-const heandleFulfilledAdd = (state, {payload}) => {
-    state.shopping.isLoading = false;
-    state.shopping.items = [...payload];
-    state.shopping.error = '';
-}
-
-
-const handlePending = state => {
-    state.shopping.isLoading = true;
+const handlePending = (state) => {
+  state.shopping.isLoading = true;
 };
 const handleRejected = (state, { payload }) => {
-    console.log('payload rejected', payload);
-    state.shopping.isLoading = false;
-    state.shopping.error = payload;
+  console.log("payload rejected", payload);
+  state.shopping.isLoading = false;
+  state.shopping.error = payload;
 };
 
 const handleFulfilledGet = (state, { payload }) => {
-    state.shopping.isLoading = false;
-    state.shopping.items = payload || state.shopping.items;
-    state.shopping.error = '';
+  state.shopping.isLoading = false;
+  state.shopping.items = payload || state.shopping.items;
+  state.shopping.error = "";
 };
 const handleFulfilledDel = (state, { payload }) => {
-    state.shopping.isLoading = false;
-    state.shopping.items = state.shopping.items.filter(el => el._id !== payload);
-    state.shopping.error = '';
-  };
-
+  state.shopping.isLoading = false;
+  state.shopping.items = state.shopping.items.filter(
+    (el) => el.ingredientId !== payload
+  );
+  state.shopping.error = "";
+};
 
 export const shoppingSlice = createSlice({
-    name:'shopping',
-    initialState:{
-        shopping:{
-            items:[],
-            isLoading:false,
-            error:null,
-        }
+  name: "shopping",
+  initialState: {
+    shopping: {
+      items: [],
+      isLoading: false,
+      error: null,
     },
-    extraReducers: builder =>{
-        builder
-            .addCase(getShoppingThunk.fulfilled, handleFulfilledGet)
-            .addCase(addShoppingThunk.fulfilled, heandleFulfilledAdd)
-            .addCase(deleteShoppingThunk.fulfilled, handleFulfilledDel)
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getShoppingThunk.fulfilled, handleFulfilledGet)
+      .addCase(addShoppingThunk.fulfilled, heandleFulfilledAdd)
+      .addCase(deleteShoppingThunk.fulfilled, handleFulfilledDel)
 
-            .addMatcher(
-                isAnyOf(
-                    addShoppingThunk.pending,
-                    getShoppingThunk.pending,
-                    deleteShoppingThunk.pending
-                ),
-                handlePending
-            )
-            .addMatcher(
-                isAnyOf(
-                    addShoppingThunk.rejected,
-                    getShoppingThunk.rejected,
-                    deleteShoppingThunk.rejected
-                ),
-                handleRejected
-            )            
+      .addMatcher(
+        isAnyOf(
+          addShoppingThunk.pending,
+          getShoppingThunk.pending,
+          deleteShoppingThunk.pending
+        ),
+        handlePending
+      )
+      .addMatcher(
+        isAnyOf(
+          addShoppingThunk.rejected,
+          getShoppingThunk.rejected,
+          deleteShoppingThunk.rejected
+        ),
+        handleRejected
+      );
+  },
+});
 
-    }
-
-})
-
-export default shoppingSlice.reducer
+export default shoppingSlice.reducer;
