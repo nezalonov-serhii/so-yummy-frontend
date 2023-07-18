86f3749f6e9515de71836fff3ca46db4ae418ed4
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-07-18 13:20:11 +0300

    CHANGE: useEffect and handleSubmit

--- src/components/CommonSearchForm/CommonSearchForm.jsx
+++ src/components/CommonSearchForm/CommonSearchForm.jsx
@@ -4,84 +4,71 @@ import { useEffect, useState, useRef } from "react";
 import { Formik } from "formik";
 import * as Yup from "yup";
 
-
-
-import { SearchFormBox, SearchFormInput, ErrorText } from "./CommonSearchForm.styled";
-
-
-// const initialValue = {
-//    query: "",
-// };
+import {
+  SearchFormBox,
+  SearchFormInput,
+  ErrorText,
+} from "./CommonSearchForm.styled";
 
 export const CommonSearchForm = ({
-   CustomButtonComponent,
-   SearchFormContainer,
-   handleSearchFormInput,
-
+  CustomButtonComponent,
+  SearchFormContainer,
+  handleSearchFormInput,
 }) => {
-const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const valueInput = searchParams.get("query");
 
-   const userSchema = Yup.object({
-      query: Yup.string()
-      .default(searchParams.get("query"))
-      .min(3,"At least 3 characters")
+  const userSchema = Yup.object({
+    query: Yup.string()
+      .default(valueInput)
+      .min(3, "At least 3 characters")
       .matches(/^[A-Za-z\s_-]+$/, "Invalid query")
       .required("Please fill field"),
-});   
+  });
 
+  const [queryValue, setQueryValue] = useState("");
+  const submitButtonRef = useRef(null);
 
+  useEffect(() => {
+    setQueryValue(valueInput);
+    if (queryValue && queryValue.length >= 3) {
+      handleSearchFormInput(queryValue);
+    }
+  }, [queryValue]);
 
-   
-   const [queryValue, setQueryValue] = useState("");
-   const submitButtonRef = useRef(null);
-  
-useEffect(() => {
-    const valueInput = searchParams.get("query")
-   
-      setQueryValue(valueInput)
-      //  setQueryValue(valueInput??queryValue)
-      if (queryValue&&queryValue.length>=3) {
-         console.log("CommonSearchForm useEffect:",queryValue)
-         handleSearchFormInput(queryValue);
-       
-        }
- 
-}, [queryValue]);
-   
-   const value = searchParams.get("query")
+  const handleSubmit = (values) => {
+    handleSearchFormInput(valueInput ?? values.query);
+    submitButtonRef.current.blur();
+  };
 
-   const handleSubmit = (values, { resetForm }) => {
-   //  console.log("onSubmit:",values.query)
-     handleSearchFormInput(value ?? values.query);
-      
-   };
-
-
-   return (
-      <SearchFormContainer>
-         <Formik initialValues={{query:""}} onSubmit={handleSubmit} validationSchema={userSchema}>
-            {({ values, handleChange, handleBlur }) => (
-               <SearchFormBox>
-                  <SearchFormInput
-                     type="text"
-                     name="query"
-                     placeholder="Enter the text"
-                     onChange={(e) => {
-                        
-                        setSearchParams({query:e.target.value })
-                        handleChange(e)
-                     }}
-                     onBlur={handleBlur}
-                     value={value??values.query}
-                     required
-                  />
-                  <ErrorText name="query" component="div" />
-                  <CustomButtonComponent type="submit" ref={submitButtonRef}>
-                     Search
-                  </CustomButtonComponent>
-               </SearchFormBox>
-            )}
-         </Formik>
-      </SearchFormContainer>
-   );
+  return (
+    <SearchFormContainer>
+      <Formik
+        initialValues={{ query: "" }}
+        onSubmit={handleSubmit}
+        validationSchema={userSchema}
+      >
+        {({ values, handleChange, handleBlur }) => (
+          <SearchFormBox>
+            <SearchFormInput
+              type="text"
+              name="query"
+              placeholder="Enter the text"
+              onChange={(e) => {
+                setSearchParams({ query: e.target.value });
+                handleChange(e);
+              }}
+              onBlur={handleBlur}
+              value={valueInput ?? values.query}
+              required
+            />
+            <ErrorText name="query" component="div" />
+            <CustomButtonComponent type="submit" ref={submitButtonRef}>
+              Search
+            </CustomButtonComponent>
+          </SearchFormBox>
+        )}
+      </Formik>
+    </SearchFormContainer>
+  );
 };
