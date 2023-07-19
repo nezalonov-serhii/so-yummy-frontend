3072ea3b2387e6ca4f155ac3531925b86fff0ef6
Sergii <nezalonov.serhii@gmail.com>
2023-07-19 18:12:04 +0300

    Update FavoritePage.jsx

--- src/page/FavoritePage/FavoritePage.jsx
+++ src/page/FavoritePage/FavoritePage.jsx
@@ -38,7 +38,7 @@ const FavoritePage = () => {
 
    const hendleDeleteRecipeById = (id) => {
       deleteRecipeFromFavorite(id);
-      setRecipes(recipes.filter((recipe) => recipe._id !== id));
+      setRecipes(recipes.filter((recipe) => recipe.favorites._id !== id));
    };
 
    return (
