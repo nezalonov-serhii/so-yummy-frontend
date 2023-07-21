9cb2baa09b5e428b5c062c99a0035a6b11c6b193
Viktoria <vik25@i.ua>
2023-07-21 20:39:12 +0200

    fix bug in popular mobile-tablet

--- src/components/AddRecipe/PopularRecipe/PopularRecipe.jsx
+++ src/components/AddRecipe/PopularRecipe/PopularRecipe.jsx
@@ -5,63 +5,79 @@ import { fetchRecipePopular } from "../../../redux/thunk/addRecipe/operations";
 
 import { Title } from "../AddRecipe.styled";
 import {
-   Container,
-   Image,
-   Item,
-   LinkId,
-   List,
-   Paragraph,
-   SecondaryTitle,
-   Text,
-   TextWrapper,
+  Container,
+  Image,
+  Item,
+  LinkId,
+  List,
+  Paragraph,
+  SecondaryTitle,
+  Text,
+  TextWrapper,
 } from "./PopularRecipe.styled";
 
 export const PopularRecipe = () => {
-   const [recipes, setRecipes] = useState([]);
-   const dispatch = useDispatch();
+  const [recipes, setRecipes] = useState([]);
+  const [itemsToShow, setItemsToShow] = useState(4);
+  const dispatch = useDispatch();
 
-   useEffect(() => {
-      const fetchData = async () => {
-         try {
-            const response = await dispatch(fetchRecipePopular());
-            const responseData = response.payload.data;
-            setRecipes(responseData);
-         } catch (error) {
-            console.error(error);
-         }
-      };
+  const updateItemsToShow = () => {
+    setItemsToShow(
+      window.innerWidth >= 768 && window.innerWidth <= 1279 ? 2 : 4
+    );
+  };
 
-      fetchData();
-   }, [dispatch]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await dispatch(fetchRecipePopular());
+        const responseData = response.payload.data;
+        setRecipes(responseData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-   return (
-      <Container>
-         <Title>Popular recipe</Title>
-         {recipes.length < 1 ? (
-            <Paragraph>There are no popular recipes at the moment.</Paragraph>
-         ) : (
-            <List>
-               {recipes
-                  .slice(0, window.innerWidth >= 768 && window.innerWidth <= 1279 ? 2 : 4)
-                  .map(({ _id, preview, title, description }) => {
-                     const name = title.length < 23 ? title : title.slice(0, 18) + "...";
-                     const paragraph =
-                        description.length < 100 ? description : description.slice(0, 80) + "...";
-                     const recipeId = _id;
-                     return (
-                        <Item key={_id}>
-                           <LinkId to={`/recipe/${recipeId}`}>
-                              <Image src={preview} />
-                              <TextWrapper>
-                                 <SecondaryTitle>{name}</SecondaryTitle>
-                                 <Text>{paragraph}</Text>
-                              </TextWrapper>
-                           </LinkId>
-                        </Item>
-                     );
-                  })}
-            </List>
-         )}
-      </Container>
-   );
+    fetchData();
+
+    window.addEventListener("resize", updateItemsToShow);
+
+    return () => {
+      window.removeEventListener("resize", updateItemsToShow);
+    };
+  }, [dispatch]);
+
+  return (
+    <Container>
+      <Title>Popular recipe</Title>
+      {recipes.length < 1 ? (
+        <Paragraph>There are no popular recipes at the moment.</Paragraph>
+      ) : (
+        <List>
+          {recipes
+            .slice(0, itemsToShow)
+            .map(({ _id, preview, title, description }) => {
+              const name =
+                title.length < 23 ? title : title.slice(0, 18) + "...";
+              const paragraph =
+                description.length < 100
+                  ? description
+                  : description.slice(0, 80) + "...";
+              const recipeId = _id;
+              return (
+                <Item key={_id}>
+                  <LinkId to={`/recipe/${recipeId}`}>
+                    <Image src={preview} />
+                    <TextWrapper>
+                      <SecondaryTitle>{name}</SecondaryTitle>
+                      <Text>{paragraph}</Text>
+                    </TextWrapper>
+                  </LinkId>
+                </Item>
+              );
+            })}
+        </List>
+      )}
+    </Container>
+  );
 };
